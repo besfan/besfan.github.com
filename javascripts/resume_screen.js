020df0f8b79a75abc99010caeb567e03c3333c67
besfan <fanxiaopeng515@163.com>
2014-03-07 20:31:41 +0800

    修改 js
    Signed-off-by: besfan <fanxiaopeng515@163.com>

--- javascripts/resume_screen.js
+++ javascripts/resume_screen.js
@@ -24,25 +24,22 @@ resume_fw.apps.init_resume_screen = function () {
                     $pic_arrow.attr("class", "arrow_show")
                 });
             });
-
+            var $return_top = $("div.return_top");
             $(window).bind("scroll", function () {
-
                 // 获取网页文档对象滚动条的垂直偏移
                 var scrollTopNum = $(document).scrollTop(),
                 // 获取浏览器当前窗口的高度
-                    winHeight = $(window).height(),
-                    return_top = $("div.return_top");
+                    winHeight = $(window).height();
 
                 // 滚动条的垂直偏移大于 0 时显示，反之隐藏
-                (scrollTopNum > 0) ? return_top.fadeIn("fast") : return_top.fadeOut("fast");
+                (scrollTopNum > 0) ? $return_top.fadeIn("fast") : $return_top.fadeOut("fast");
 
                 // 给 IE6 定位
                 if (!-[1, ] && !window.XMLHttpRequest) {
-                    return_top.css("top", scrollTopNum + winHeight - 200);
+                    $return_top.css("top", scrollTopNum + winHeight - 200);
                 }
-
             });
-            $("div.return_top").hover(
+            $return_top.hover(
                 function () {
                     $(this).addClass("return_top_hover");
                 },
@@ -50,7 +47,7 @@ resume_fw.apps.init_resume_screen = function () {
                     $(this).removeClass("return_top_hover");
                 });
             // 点击按钮后，滚动条的垂直方向的值逐渐变为0，也就是滑动向上的效果
-            $("div.return_top").click(function () {
+            $return_top.click(function () {
                 $("html, body").animate({ scrollTop: 0 }, 500);
             });
         }
