bd1a427e37ad78cac99fd8fc63dda0d72b06fb88
besfan <fanxiaopeng515@163.com>
2014-03-08 17:00:29 +0800

    new demo
    新建一个rails 工程

---
+++ work/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '93528aa633c43a1554e4fbe32ef5892804ac397c295a1a942e682f1d852a4dd3e06c9a101a16c7b0a339a8e0f6fd9ce4d114429a541af5b82f1035b1c90296ef'
